d2e21a7800873ff1f6a3256baa9d51b911d85dc7
Nztzlie <nataliebaer@Natalies-MacBook-Pro.local>
2014-04-06 08:33:15 -0400

    Merge branch 'master' of https://github.com/Nztzlie/phase_0_unit_2
    Conflicts:
    week_4/my_solutions/4_group_cipher_solution.rb

---
+++ week_4/my_solutions/4_group_cipher_solution.rb
@@ -0,0 +1,122 @@
+# U2.W4: Cipher Challenge
+
+
+# I worked on this challenge with Nick Giovacchini
+
+
+
+# 1. Solution
+# Write your comments on what each thing is doing. 
+# If you have difficulty, go into IRB and play with the methods.
+
+def north_korean_cipher(coded_message)
+  input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the ruby docs.
+  #This splits the strings, so that each key is an individual string.
+  decoded_sentence = []
+  cipher = {"e" => "a",   # This is technically a shift of four letters...Can you think of a way to automate this? Is a hash
+            "f" => "b",   # the best data structure for this problem? What are the pros and cons of hashes?
+            "g" => "c",   # Hashes do not have location, so shifting four spaces needs to be coded into the hash,
+            "h" => "d",   # unlike with an array.
+            "i" => "e", 
+            "j" => "f",
+            "k" => "g",
+            "l" => "h",
+            "m" => "i",
+            "n" => "j",
+            "o" => "k",
+            "p" => "l",
+            "q" => "m",
+            "r" => "n",
+            "s" => "o",
+            "t" => "p",
+            "u" => "q",
+            "v" => "r",
+            "w" => "s",
+            "x" => "t",
+            "y" => "u",
+            "z" => "v",
+            "a" => "w",
+            "b" => "x",
+            "c" => "y",
+            "d" => "z"}
+            
+  input.each do |x| # What is #each doing here? # Iterating through each string value and assigning the value x to each character in turn. 
+    found_match = false  # Why would this be assigned to false from the outset? What happens when it's true? # False because it hasn't gone through the cipher yet so it doesn't the expected unencrypted outcome yet. 
+    cipher.each_key do |y| # What is #each_key doing here? # It is assigning a key value that corresponds to the input. 
+      if x == y  # What is this comparing? Where is it getting x? Where is it getting y? What are those variables really? # It is comparing if the input value is equal to the key value. It tracks if x is the same as y. Those values are really a split version of the input value. 
+        puts "I am comparing x and y. X is #{x} and Y is #{y}."
+        decoded_sentence << cipher[y]
+        found_match = true
+        break  # Why is it breaking here? This breaks to prevent infinite looping.
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing? 
+        # these characters act as spaces. if target equals any of these symbols, it outputs " " (a space)
+        decoded_sentence << " "
+        found_match = true
+        break
+      elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do?
+          # => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        decoded_sentence << x
+        found_match = true
+        break
+      end 
+    end
+    if not found_match  # What is this looking for? # This is looking for instances not covered in examples above. If its not found in the cipher it simply returns itself. 
+      decoded_sentence << x
+    end
+  end
+  decoded_sentence = decoded_sentence.join("")
+ 
+  if decoded_sentence.match(/\d+/) #What is this matching? Look at Rubular for help. #This is finding digits 
+    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate... 
+  # This takes digits found, and divides them by 100.
+  end  
+  return decoded_sentence # What is this returning? # This is the output value which is == decoded_sentence.join("")   
+end
+
+# Your Refactored Solution
+def north_korean_cipher(coded_message)
+  input = coded_message.downcase.split("")
+  decoded_sentence = []
+  cipher = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+  input.each do |x|
+    # code = 0
+    found_match = false
+    code = 0
+    cipher.each do |y|
+      if x == y
+        decoded_sentence << cipher[code-4]
+        found_match = true
+        # code +=1
+        break
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*"
+        decoded_sentence << " "
+        found_match = true
+        break
+      end
+      code += 1
+    end
+    if not found_match
+      decoded_sentence << x
+    end
+  end
+  decoded_sentence = decoded_sentence.join("")
+
+  if decoded_sentence.match(/\d+/)
+    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 }
+  end
+  return decoded_sentence
+end
+
+# Driver Code:
+p north_korean_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver code and should print true
+# Find out what Kim Jong Un is saying below and turn it into driver code as well. Driver Code statements should always return "true"
+p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.")
+p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!")
+p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.")
+p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!")
+p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?")
+
+# Reflection
+#This article was interesting, getting the alphabet to automatically move four spots was an interesting challenge
+# Had a good pairing session for this one.
--- week_4/my_solutions/5_reverse_cipher_solution.rb
+++ week_4/my_solutions/5_reverse_cipher_solution.rb
@@ -1,8 +1,8 @@
 # U2.W4: Refactor Cipher Solution
 
 
-# I worked on this challenge [by myself, with: ].
-
+# I worked on this challenge:
+Indigo Nai and Natalie Baer
 
 # 1. Solution
 # Write your comments on what each thing is doing. 
@@ -32,26 +32,27 @@ end
 
 
 # Questions:
-# 1. What is the .to_a method doing?
-# 2. How does the rotate method work? What does it work on?
-# 3. What is `each_char` doing?
-# 4. What does `sample` do?
-# 5. Are there any other methods you want to understand better?
-# 6. Does this code look better or worse than your refactored solution
-#    of the original cipher code? What's better? What's worse?
-# 7. Is this good code? What makes it good? What makes it bad?
-
+=begin
+1. What is the .to_a method doing? This method is taking the ('a'..'z') and making it into an array.
+2. How does the rotate method work? What does it work on? Allows it to rotate through array (as though it were a wheel)
+back to the beginning and shift that number of spaces (in this case, 4).
+3. What is `each_char` doing? It is going over each character in the string and doing something at each character.
+4. What does `sample` do? Sample is the same as saying "choose something random from array", in this case spaces.
+5. Are there any other methods you want to understand better? this one: cipher = Hash[alphabet.zip(alphabet.rotate(4))]
+6. Does this code look better or worse than your refactored solution of the original cipher code? What's better? What's worse?
+7. Is this good code? What makes it good? What makes it bad? Its relatively easy to follow and relatively succinct, which is good.  It follows principles of DRY, naming conventions are good.  This code, however, is only re-usable for this particular cipher pattern, not for others, so it might be a bit simplistic.
+=end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # Does this return the same thing every time?
-p translate_to_cipher("I want cookies")
-p translate_to_cipher("I want cookies")
-p translate_to_cipher("I want cookies")
-p translate_to_cipher("I want cookies")
 
+p translate_to_cipher("I want cookies")
+p translate_to_cipher("I want cookies")
+p translate_to_cipher("I want cookies")
+p translate_to_cipher("I want cookies")
 
 
 
 
 # 5. Reflection 
-
+# It was helpful to me to see this compact and relatively readable code as a good example.
