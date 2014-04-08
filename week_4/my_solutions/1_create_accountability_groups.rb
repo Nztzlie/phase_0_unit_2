431721a546dd2e025f6a68e6e804306e1258538e
Nztzlie <nataliebaer@Natalies-MacBook-Pro.local>
2014-04-08 12:31:43 -0400

    Syncing updates.

--- week_4/my_solutions/1_create_accountability_groups.rb
+++ week_4/my_solutions/1_create_accountability_groups.rb
@@ -13,7 +13,7 @@ Output: For each array, accountability groups of four, with extra people distrib
 Steps:
 Write driver code to find the number of elements in each array, and test whether each is divisible by four
 Write driver code to print groups when they are established, to see whether they are in groups of four or more
- 
+
 Check to see whether the total number of elements in the array is divisible by four
 If divisible by four, then randomly sort into groups of four
 If not divisible by four, then sort into groups of four until last group
@@ -25,17 +25,25 @@ If members in the array are <3, they will be pushed into established arrays
 
 # 3. Initial Solution
 
-
-
-
-# 4. Refactored Solution
 def account(a)
 		groups = a.shuffle.each_slice(4).to_a
+    p groups
 		if groups[-1].length == 1
 			groups[0] << groups[-1]
 			groups.inspect
 		end
 end
+
+a = [ "Ken Sin", "Dinesh Rai", "Charu Sharma", "David Kerr" , "Chantelle Turnbull", "Jake Huhn", "Samuel Arkless",
+			"Kai Prout", "Indigo", "Timothy McClung", "Judy Jow", "Danielle Adams", "John P Quigley", "Adeoye Jaiyeola",
+			"Lasse Sviland", "Jessica Tatham", "Kevin Zhou", "Kennedy Bhagwandeen", "Jason Matney", "Gregory Knudsen",
+			"Natalie Baer", "Nick Giovacchini"]
+
+# initial driver:
+print account(a)
+
+
+# 4. Refactored Solution
 
 a = [ "Ken Sin", "Dinesh Rai", "Charu Sharma", "David Kerr" , "Chantelle Turnbull", "Jake Huhn", "Samuel Arkless", 
 			"Kai Prout", "Indigo", "Timothy McClung", "Judy Jow", "Danielle Adams", "John P Quigley", "Adeoye Jaiyeola", 
@@ -45,7 +53,7 @@ a = [ "Ken Sin", "Dinesh Rai", "Charu Sharma", "David Kerr" , "Chantelle Turnbul
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-print account(a)
+
 
 
 
